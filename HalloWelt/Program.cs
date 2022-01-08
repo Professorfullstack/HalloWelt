5ffbd0a8565e23f3cd03ed698951f5a8aa7930d9
Professorfullstack <mail@vks.info>
2022-01-08 18:51:58 +0100

    Projektdateien hinzufügen.

---
+++ HalloWelt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HalloWelt
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            int count2 = 4; //Anzahl der Reihen
+            int count = 0; //Zähler
+
+            for (int i = count; i < count2-1; i++)
+            {
+                Console.WriteLine("L");
+                count +=1;
+
+            }
+
+            if (count == count2-1)
+            {
+                for (int k = 0; k < count2; k++)
+                {
+                    Console.Write("L");
+
+                }               
+            }      
+        }
+    }
+}
